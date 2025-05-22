942b82fe9386d5ca6ce4fdaebb9c1dce1fb3eb05
Sooyoung <tr248267@gmail.com>
2025-05-22 12:07:06 +0900

    책표지 표시

--- book/views.py
+++ book/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from book.models import BookInfo
 
 # Create your views here.
 def best_seller(request):
-    return render(request, "book/best_seller.html")
+    # 모델이 학습한 베스트셀러를 반환 (추후 요수정)
+    all_book = BookInfo.objects.all()
+    context = {
+        "all_book" : all_book,
+    }
+
+    return render(request, "book/best_seller.html", context)
