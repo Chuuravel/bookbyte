c9aa4f90987716bfbed6e0fe7174c63649e1e221
SooyoungJung <tr248267@gmail.com>
2025-06-11 20:59:22 +0900

    bug fix-detail page

--- book/urls.py
+++ book/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("bestseller/", views.best_seller, name="best_seller"),
     path("recmdbook/", views.recommended_book, name="recommended_book"),
     path("showall/", views.show_all_book, name = "show_all_book"),
-    path("detail/<str:isbn>/", views.detail, name="detail"),
-    path("create/<str:isbn>/", views.create_review, name="create_review"),
+    path("detail/<int:pk>/", views.detail, name="detail"),
+    path("create/<int:pk>/", views.create_review, name="create_review"),
     path("showreview/", views.show_review, name="show_review")
 ]
